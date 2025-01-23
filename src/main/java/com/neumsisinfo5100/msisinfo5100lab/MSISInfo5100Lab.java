8b9c79bbe8ed30315bbda5ab0800f2d717a3fcd4
Prasanth Jayabal <jayabal.pr@northeastern.edu>
2025-01-23 01:16:14 -0500

    Info 5100 Lab Assignment-1

---
+++ src/main/java/com/neumsisinfo5100/msisinfo5100lab/MSISInfo5100Lab.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.neumsisinfo5100.msisinfo5100lab;
+
+/**
+ *
+ * @author prasanthj
+ */
+public class MSISInfo5100Lab {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
