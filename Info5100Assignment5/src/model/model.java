cf43670f5bfac46ad547e38efc88605eb875ec08
Prasanth-Jayabal <jayabal.pr@northeastern.edu>
2025-02-21 23:38:28 -0500

    Add files via upload
    Info 5100 lab assignment 5

---
+++ Info5100Assignment5/src/model/model.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author prasanthj
+ */
+public class model {
+    
+    private String name;
+    private int age;
+    private String email;
+
+    public model(String name, int age, String email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    
+}
